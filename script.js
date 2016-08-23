494def42383ad1ea6c600f3c4c86c063dbbd895d
Matt Aitchison <Matt@lanciv.com>
2016-08-23 17:56:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function onPush(github, event, cb) {
   console.log("Hello world");
   cb();
 }
+
+
+function onPullRequestReviewComment(github, event, cb) {
+	console.log(event.comment.path);
+	cb();
+}
