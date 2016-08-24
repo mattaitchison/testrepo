0891b82bf4beebd45669c955adf210f8070f3246
Matt Aitchison <Matt@lanciv.com>
2016-08-24 14:13:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function onIssueComment(github, event, cb) {
     var repo = event.repository.name;
     var author = event.comment.user.login;
     var msg = event.comment.body;
-    if (msg.toLowerCase() !== 'lgtm' || event.issue.state !== 'open') {
-        console.log("nothing to do");
+    if (msg.toLowerCase() !== 'lgtm') {
+        console.log("message isn't lgtm");
+        return cb();
+    }
+    if (event.issue.state !== 'open') {
+        console.log("issue isn't open");
         return cb();
     }
     github.repos.checkCollaborator({
@@ -24,9 +28,19 @@ function onIssueComment(github, event, cb) {
         });
     }, function (err) {
         console.log(JSON.stringify({ "error": err }));
+        throw new Error("author should be collaborator to merge pr");
     }).then(function (res) {
         console.log(JSON.stringify(res));
     }, function (err) {
         console.log(JSON.stringify({ "error": err }));
-    }).then(cb);
+        if (err.code == 405) {
+            throw new Error("Pull Request is not mergeable");
+        }
+        if (err.code == 404) {
+            throw new Error("PR for issue# not found");
+        }
+    }).then(cb).catch(function (e) {
+        console.log("caught err", e);
+        cb();
+    });
 }
