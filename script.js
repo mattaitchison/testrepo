a66ab0ab3b2fc2231a7faaa88f9f5c61b64e33e6
Matt Aitchison <Matt@lanciv.com>
2016-08-24 14:18:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function onPush(github, event, cb) {
-    console.log('Hello world');
-    cb();
-}
 function onIssueComment(github, event, cb) {
-    var owner = event.repository.owner.login;
-    var repo = event.repository.name;
-    var author = event.comment.user.login;
-    var msg = event.comment.body;
+    const owner = event.repository.owner.login;
+    const repo = event.repository.name;
+    const author = event.comment.user.login;
+    const msg = event.comment.body;
     if (msg.toLowerCase() !== 'lgtm') {
         console.log("message isn't lgtm");
         return cb();
@@ -15,31 +11,41 @@ function onIssueComment(github, event, cb) {
         console.log("issue isn't open");
         return cb();
     }
+
     github.repos.checkCollaborator({
         user: owner,
         repo: repo,
         collabuser: author
-    }).then(function (res) {
-        return github.pullRequests.merge({
-            user: owner,
-            repo: repo,
-            number: event.issue.number,
-            squash: true
-        });
-    }, function (err) {
-        console.log(JSON.stringify({ "error": err }));
-        throw new Error("author should be collaborator to merge pr");
-    }).then(function (res) {
-        console.log(JSON.stringify(res));
-    }, function (err) {
-        console.log(JSON.stringify({ "error": err }));
-        if (err.code == 405) {
-            throw new Error("Pull Request is not mergeable");
+    }).then(
+        res => {
+            return github.pullRequests.merge({
+                user: owner,
+                repo: repo,
+                number: event.issue.number,
+                squash: true
+            });
+        },
+        err => {
+            console.log(JSON.stringify({
+                "error": err
+            }));
+            throw new Error("author should be collaborator to merge pr");
+    }).then(
+        res => {
+            console.log(JSON.stringify(res))
+        },
+        err => {
+            console.log(JSON.stringify({
+                "error": err
+            }));
+            if (err.code == 405) {
+                throw new Error("Pull Request is not mergeable");
+            }
+            if (err.code == 404) {
+                throw new Error("PR for issue number not found");
+            }
         }
-        if (err.code == 404) {
-            throw new Error("PR for issue number not found");
-        }
-    }).then(cb).catch(function (e) {
+    ).then(cb).catch(e => {
         console.log("caught err", e);
         cb();
     });
