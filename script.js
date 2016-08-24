f29fec1b7bdf3242708e0c17f5aab0add3d8b609
Matt Aitchison <Matt@lanciv.com>
2016-08-24 12:28:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function onIssueComment(github, event, cb) {
 	console.log(event.repository.owner.login)
 	console.log(event.repository.name)
 	console.log(event.issue.number)
+	console.log(event.comment)
 	// console.log(JSON.stringify(event));
 	github.pullRequests.get({
     user: event.repository.owner.login,
     repo: event.repository.name,
     number: event.issue.number
   }).then(function(a,b){
-		console.log(JSON.stringify(a),JSON.stringify(b));
+		// console.log(JSON.stringify(a),JSON.stringify(b));
 		cb();
 	}).catch(function(e) {
     console.log(e);
