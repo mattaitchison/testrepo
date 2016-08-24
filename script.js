44d0ba83c8797a0c9df978b62aa09f9ad7d324e1
Matt Aitchison <Matt@lanciv.com>
2016-08-24 13:58:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function onIssueComment(github, event, cb) {
     github.repos.checkCollaborator({
         user: owner,
         repo: repo,
-        collabuser: author
+        collabuser: "progirum"
     }).then(function (res) {
         return github.pullRequests.merge({
             user: owner,
